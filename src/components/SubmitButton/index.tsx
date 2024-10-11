c1b8ff0d8e7c03431364f7d365e669626c4b4207
FelipeTomazetti <felipe.pistori@gmail.com>
2024-10-11 09:24:18 -0300

    [FT] - feat: cria componente do botao de submit para o formulario

---
+++ src/components/SubmitButton/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface SubmitButtonProps {
+  label: string;
+}
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({ label }) => {
+  return (
+    <button
+      type="submit"
+      className="w-80 bg-cyan-600 text-white py-2 rounded hover:bg-cyan-500 transition duration-300 mt-2"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default SubmitButton;
