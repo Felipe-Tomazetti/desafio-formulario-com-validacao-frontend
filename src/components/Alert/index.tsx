d81d602b896b9885e5353463499f5f6007ade56c
FelipeTomazetti <felipe.pistori@gmail.com>
2024-10-11 11:56:10 -0300

    [FT] - chore: ajustes na parte visual

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 interface AlertProps {
-  mensagem: string | null;
+  message: string | null;
 }
 
-const Alert: React.FC<AlertProps> = ({ mensagem }) => {
-  if (!mensagem) return null;
+const Alert: React.FC<AlertProps> = ({ message }) => {
+  if (!message) return null;
 
   return (
     <div
       className={`mb-4 p-2 text-center ${
-        mensagem.includes("sucesso")
+        message.includes("sucesso")
           ? "bg-green-100 text-green-700"
           : "bg-red-100 text-red-700"
       } rounded`}
     >
-      {mensagem}
+      {message}
     </div>
   );
 };
