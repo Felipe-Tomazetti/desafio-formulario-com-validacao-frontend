e7d8ceb2b3b59c45632b1c9292eabe2bba7ff041
FelipeTomazetti <felipe.pistori@gmail.com>
2024-10-11 21:37:06 -0300

    [PD-FT] 0216 - fix: correcao de layout do botao em mobile

--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -8,7 +8,7 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({ label }) => {
   return (
     <button
       type="submit"
-      className="w-80 bg-cyan-600 text-white py-2 rounded hover:bg-cyan-500 transition duration-300 mt-2"
+      className="w-44 md:w-80 bg-cyan-600 text-white py-2 rounded hover:bg-cyan-500 transition duration-300 mt-2"
     >
       {label}
     </button>
